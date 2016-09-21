35da379dbffb28a844e753b579ed398c2f975ff7
SH Hridoy <shhridoy.cste@gmail.com>
2016-09-21 20:13:24 +0600

    Uri

---
+++ 1006 - Average 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    double a, b, c, MEDIA;
+
+    scanf("%lf %lf %lf",&a, &b, &c);
+
+    MEDIA = ((a*2) + (b*3) + (c*5))  / (2+3+5);
+
+    printf("MEDIA = %.1lf\n",MEDIA);
+
+    return 0;
+}
