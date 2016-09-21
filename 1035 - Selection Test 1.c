2cf1773139e9b7fb17da33511e94dacc7ea896bf
SH Hridoy <shhridoy.cste@gmail.com>
2016-09-21 20:35:58 +0600

    Create 1035 - Selection Test 1.c

---
+++ 1035 - Selection Test 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+    int a,b,c,d,sum1,sum2,e,f;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    sum1=c+d;
+    sum2=a+b;
+    if(b>c && d>a && sum1>sum2 && c>=0 && d>=0 && a%2==0){
+        printf("Valores aceitos\n");
+    }
+    else
+        printf("Valores nao aceitos\n");
+ 
+    return 0;
+}
