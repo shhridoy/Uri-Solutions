1e7fba8e8c48882b7e509af02a2d2fa018b8fc69
SH Hridoy <shhridoy.cste@gmail.com>
2016-09-21 20:29:35 +0600

    Create 1020 - Age in Days.c

---
+++ 1020 - Age in Days.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+     int n,year,month,day;
+    scanf("%d",&n);
+    year=n/365;
+    month=n%365/30;
+    day=n%365%30;
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n",year,month,day);
+ 
+    return 0;
+}
