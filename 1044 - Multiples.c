8c8bfdcd3527b206f0dc46d1d31f5d45ca8e989a
SH Hridoy <shhridoy.cste@gmail.com>
2017-01-25 14:21:28 +0600

    Create 1044 - Multiples.c

---
+++ 1044 - Multiples.c
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int a, b;
+
+    cin>> a >> b;
+
+    if( b%a == 0 || a%b ==0)
+        printf("Sao Multiplos\n");
+
+    else
+        printf("Nao sao Multiplos\n");
+ 
+    return 0;
+}
