47969222b1f7d4e37d035571ccd7791ccddba21b
SH Hridoy <shhridoy.cste@gmail.com>
2017-02-01 20:28:58 +0600

    Create 1074 - Even or Odd.c

---
+++ 1074 - Even or Odd.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+     int q, n;
+ scanf("%d", &q);
+ while(q--){
+ scanf("%d", &n);
+ if (n == 0){
+ printf("NULL\n");
+ continue;
+ }
+
+if (n % 2 == 0)
+ printf("EVEN ");
+ else
+ printf("ODD ");
+
+if (n < 0)
+ printf("NEGATIVE\n");
+ else
+ printf("POSITIVE\n");
+ }
+
+return 0;
+}
