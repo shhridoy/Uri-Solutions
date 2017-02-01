3159bb6e3d6f8c74bc025e2e35c6f61054c30c2c
SH Hridoy <shhridoy.cste@gmail.com>
2017-02-01 20:32:56 +0600

    Create 1095 - Sequence IJ 1.c

---
+++ 1095 - Sequence IJ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int i, j;
+    for(i=1, j=60; i<=j, j>=0; i=i+3, j=j-5){
+            printf("I=%d J=%d\n", i, j);
+        while(j==0)
+            break;
+    }
+ 
+    return 0;
+}
