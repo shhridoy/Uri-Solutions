e06db1554d8dd3c7123b7f0388a586e2fc11b273
SH Hridoy <shhridoy.cste@gmail.com>
2017-02-05 22:43:24 +0600

    Create 1114 - Fixed Password.c

---
+++ 1114 - Fixed Password.c
@@ -0,0 +1,22 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int a;
+    while(1)
+    {
+        cin >> a;
+        if(a == 2002){
+            cout << "Acesso Permitido" <<endl;
+            break;
+        }
+        else
+        {
+            cout << "Senha Invalida" <<endl;
+        }
+    }
+ 
+    return 0;
+}
